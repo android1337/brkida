8221845f4a1908227448b6bfd1ac12b9850ddca6
Android1337 <51016908+Android1337@users.noreply.github.com>
2024-04-12 23:10:51 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "brkida.hpp"
 
 int main() {
-    BRKIDA; // define this inside every function you want to break
+    BRKIDA; // define this at the start of every function you want to break
 
     printf("Hello!\n");
 
